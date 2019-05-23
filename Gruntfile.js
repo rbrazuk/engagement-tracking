65ab4802da64b4d1eca1210038f5976721d7fd7a
Ross Brazuk <ross.brazuk@gmail.com>
2019-05-23 12:31:00 -0400

    Fixed error in gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -67,25 +67,25 @@ module.exports = function (grunt) {
           '.tmp/styles/{,*/}*.css',
           '<%= yeoman.app %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}'
         ]
+      }
+    },
+    buildcontrol: {
+      options: {
+        dir: 'dist',
+        commit: true,
+        push: true,
+        message: 'Built %sourceName% from commit %sourceCommit% on branch %sourceBranch%'
       },
-      buildcontrol: {
+      pages: {
         options: {
-          dir: 'dist',
-          commit: true,
-          push: true,
-          message: 'Built %sourceName% from commit %sourceCommit% on branch %sourceBranch%'
-        },
-        pages: {
-          options: {
-            remote: 'git@github.com:rbrazuk/engagement-tracking.git',
-            branch: 'gh-pages'
-          }
-        },
-        local: {
-          options: {
-            remote: '../',
-            branch: 'build'
-          }
+          remote: 'git@github.com:rbrazuk/engagement-tracking.git',
+          branch: 'gh-pages'
+        }
+      },
+      local: {
+        options: {
+          remote: '../',
+          branch: 'build'
         }
       }
     },
